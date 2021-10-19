6792776064c7c928a9ed0e1c6bbf9c71291bcd0c
iamdual <imduual@gmail.com>
2021-10-19 10:10:34 +0300

    use global value

--- app/Console/clearCache.php
+++ app/Console/clearCache.php
@@ -1,11 +1,12 @@
 <?php namespace App\Console;
 
+use Sonic\Config\Environment;
 use Sonic\Console;
 
 class clearCache extends Console
 {
     public function handler(...$args): void
     {
-        @unlink(SONIC . '/.env.cached.php');
+        @unlink(Environment::$cached_file);
     }
 }
