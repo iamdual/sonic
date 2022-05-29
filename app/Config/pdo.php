2c0019d518360a39eae762c9ce9044468a83f812
Ekin Karadeniz <iamdual@icloud.com>
2022-05-29 13:15:23 +0300

    rename env fields

--- app/Config/pdo.php
+++ app/Config/pdo.php
@@ -2,9 +2,9 @@
 
 return [
     'default' => [
-        'pdo_dsn' => getenv('DB_PDO_DSN') ?:
+        'pdo_dsn' => getenv('PGSQL_DSN') ?:
             'pgsql:host=localhost;port=5432;dbname=sonic;user=dual;password=1234',
-        'username' => getenv('DB_USERNAME') ?: null,
-        'password' => getenv('DB_PASSWORD') ?: null,
+        'username' => getenv('PGSQL_USERNAME') ?: null,
+        'password' => getenv('PGSQL_PASSWORD') ?: null,
     ],
 ];
